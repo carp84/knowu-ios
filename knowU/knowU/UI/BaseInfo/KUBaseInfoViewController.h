d85a63f67295a3e11b44d81ffc54e63653c0735e
HanJiatong <hanjiatong@lianjia.com>
2015-05-10 18:59:08 +0800

    详细资料UI

---
+++ knowU/knowU/UI/BaseInfo/KUBaseInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  KUBaseInfoViewController.h
+//  knowU
+//
+//  Created by HanJiatong on 15/5/10.
+//  Copyright (c) 2015年 HanJiatong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KUBaseInfoViewController : UIViewController
+
+@end
---
+++ knowU/knowU/UI/DetailInfo/KUDetailInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  KUDetailInfoViewController.h
+//  knowU
+//
+//  Created by HanJiatong on 15/5/10.
+//  Copyright (c) 2015年 HanJiatong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KUDetailInfoViewController : UIViewController
+
+@end
